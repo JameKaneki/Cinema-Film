7f7a322317af731942b5dd694349dc153bf9f023
tiesn28 <tiennvph30519@fpt.edu.vn>
2023-07-14 19:56:19 +0700

    feat/modules-cinema-ticket:update

--- modules/cinema.php
+++ modules/cinema.php
@@ -1,5 +1,14 @@
 <?php
     function insert_cinema($nameCinema,$addressCinema){
-        
+        $sql= "INSERT INTO `cinema`(`nameCinema`, `addressCinema`) VALUES ('$nameCinema','$addressCinema')";
+        pdo_execute($sql);
+    }
+    function delete_cinema($idCinema){
+        $sql= "DELETE FROM `cinema` WHERE idCinema='$idCinema'";
+        pdo_execute($sql);
+    }
+    function update_cinema($idCinema,$nameCinema,$addressCinema){
+        $sql= "UPDATE `cinema` SET `nameCinema`='$nameCinema',`addressCinema`='$addressCinema' WHERE `idCinema`='$idCinema'";
+        pdo_execute($sql);
     }
 ?>
--- modules/ticket.php
+++ modules/ticket.php
@@ -1,5 +1,14 @@
 <?php
     function insert_ticket($idSchedules,$price,$showTime,$seatKey,$idRoom,$idUser){
-        
+        $sql="INSERT INTO `ticket (`idSchedules`, `price`, `showTime`, `seatKey`, `idRoom`, `idUser`) VALUES ('$idSchedules','$price','$showTime','$seatKey','$idRoom','$idUser')";
+        pdo_execute($sql);
+    }
+    function delete_ticket($idTicket){
+        $sql="DELETE FROM `ticket` WHERE `idTicket`='$ticket'";
+        pdo_execute($sql);
+    }
+    function update_ticket($idTicket,$idSchedules,$price,$showTime,$seatKey,$idRoom,$idUser){
+        $sql="UPDATE `ticket` SET `idSchedules`='$idSchedules',`price`='$price',`showTime`='$showTime',`seatKey`='$seatKey',`idRoom`='$idRoom',`idUser`='$idUser' WHERE `ticket`='$idTicket'";
+        pdo_execute($sql);
     }
 ?>
