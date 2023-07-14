82c301e95cc3acc07c180239038a7a5e61dd4bd3
JameNguyen <choitaosao1999@gmail.com>
2023-07-14 22:25:27 +0700

    hot fix base

--- modules/user.php
+++ modules/user.php
@@ -5,13 +5,13 @@ function insert_user($userName,$password,$email,$name,$phoneNumber,$address)
     pdo_execute($sql);
 }
 
-function update_acount($idUser,$userName,$password,$email,$name,$phoneNumber,$address){
+function update_account($idUser,$userName,$password,$email,$name,$phoneNumber,$address){
     $sql="UPDATE `user` SET `userName`='$userName',`password`='$password',`email`='$email',`name`='$name',
     `phoneNumber`='$phoneNumber',`address`='$address' WHERE `idUser`='$idUser'";
     pdo_execute($sql);
 }
 
-function delete_acount($idUser){
+function delete_account($idUser){
     $sql = "DELETE FROM `user` WHERE `idUser`=".$idUser;
     pdo_execute($sql);
 }
--- views/admin/header.php
+++ views/admin/header.php
@@ -18,13 +18,17 @@
         </div>
         <div class="row mb menu">
             <ul>
+            <!-- phần act này cần clear để mn cùng lamf việc 
+                - act = <cờ> :
+                    + cờ ở đây sẽ bao gồm tên bảng, chức năng sẽ sử dụng ví dụ : act=film-add, act=film-update
+                việc này hơi mất công một chút nhưng nó sẽ giups cả nhóm thống nhất cách hoatj động     
+            -->
                 <li><a href="index.php?act=home">Home</a></li>
-                <li><a href="index.php?act=adddm">Quản lý danh mục phim</a></li>
-                <li><a href="index.php?act=addsp">Quản lý phim</a></li>
-                <li><a href="index.php?act=dskh">Quản lý user</a></li>
-                <li><a href="index.php?act=dsbl">Quản lý room</a></li>
-                <li><a href="index.php?act=thongke">Quản lý Schedules</a></li>
-                <li><a href="index.php?act=listbill">Quản lý ticket</a></li>
-                <li><a href="index.php?act=listbill">Quản lý cinema</a></li>
+                <li><a href="index.php?act=film">Film</a></li>
+                <li><a href="index.php?act=user">User</a></li>
+                <li><a href="index.php?act=room">Room</a></li>
+                <li><a href="index.php?act=schedule">Schedules</a></li>
+                <li><a href="index.php?act=ticket">Ticket</a></li>
+                <li><a href="index.php?act=cinema">Cinema</a></li>
             </ul>
         </div>
